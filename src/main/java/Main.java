eea8c62bb7efebfda2b445409edfef3c5cc48bfc
Antonio <atomas@elpuig.xeill.net>
2021-03-18 17:20:56 +0100

    Added sprite with dummy movement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,7 +11,17 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("SpaceGame");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("GameWindow.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
 
     }
 }
